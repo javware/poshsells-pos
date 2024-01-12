5452842dbed140ede076dce70c95d1bcc7b8841b
JavierAT <xaviercito052018@gmail.com>
2024-01-12 08:20:18 -0500

    diseño final pos

--- modules/pos/tests.py
+++ modules/pos/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import json
+import ast
+employee_string = "{'success': False, 'error': None, 'error_message': 'Error en la generación de factura/boleta desde Nubefact', 'response_nubefact': {'errors': 'Este documento ya existe en NubeFacT', 'codigo': 23}}"
+employee_string1 = ""
+employee_dict = json.dumps(ast.literal_eval(employee_string))
+employee_dict = json.loads(employee_dict)
 
-# Create your tests here.
+if employee_string != "":
+    if not employee_dict['success']:
+        print("true")
+
+
+
+
